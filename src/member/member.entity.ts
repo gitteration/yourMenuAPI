3eb852a28e1c0ea8acaf938299573f80c6386703
gitteration <show10033@naver.com>
2023-02-12 16:22:56 +0900

    feat: member type 추가 적용

--- src/member/member.entity.ts
+++ src/member/member.entity.ts
@@ -2,6 +2,22 @@ import { Entity, Column } from 'typeorm';
 import {BaseEntity} from "../entities/base-entity";
 import {Field, Float, ObjectType} from "@nestjs/graphql";
 
+
+enum WEATHER {
+    HOT = 'hot',        // 더운 날
+    COLD = 'cold',      // 추운 날
+    SUNNY = 'sunny',    // 화창한 날
+    CLEAR = 'clear',    // 청명한 날
+    DRY = 'dry',        // 건조한 날
+    FOGGY = 'foggy',    // 안개낀 날
+    RAINY = 'rainy',    // 비오는 날
+    THUNDER = 'thunder' // 번개치는 날
+}
+enum GENDER {
+    M = 'm',    // 남자
+    W = 'w',    // 여자
+    N = 'n'     // 남자도 여자도 아닌 중성
+}
 @ObjectType()
 @Entity()
 export class Member extends BaseEntity{
@@ -9,10 +25,10 @@ export class Member extends BaseEntity{
     @Column({
         type: 'varchar',
         length: 20,
-        comment: '날씨',
+        comment: `날씨`,
         nullable: false,
     })
-    weather: string;
+    weather: WEATHER;
 
     @Field(() => Float)
     @Column({
@@ -32,16 +48,15 @@ export class Member extends BaseEntity{
         comment: '성별(W,M,N)',
         nullable: false,
     })
-    gender: string;
+    gender: GENDER;
 
     @Field(() => Number)
     @Column({
         type: 'varchar',
         length: 20,
-        comment: '핸드폰 번호',
+        comment: '핸드폰 번호(- 없이)',
         nullable: false,
     })
     phone_number: number;
-
 
 }
