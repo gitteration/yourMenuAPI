40c837c412e5d5d312a88e093e4836c9320d72c4
gitteration <show10033@naver.com>
2023-02-22 00:03:53 +0900

    feat: synchronize false로 설정

--- src/entities/delete-entity.ts
+++ src/entities/delete-entity.ts
@@ -1,6 +1,7 @@
 import {Field, Int, ObjectType} from "@nestjs/graphql";
 
 @ObjectType('DeleteEntity')
+//TODO: 삭제 엔티티를 만드는게 맞을까?? 후에 리펙토링하면서 생각해보자
 export class DeleteEntity{
     @Field(()=>[String])
     raw:any
